86ef77cb2675b939be514566ec4384ab7e94f010
Carlos Vera <carlosvera@veradesign.es>
2018-11-19 14:45:23 +0100

    Configuraciones Varias

--- contactme/configs.php
+++ contactme/configs.php
@@ -3,24 +3,24 @@
 error_reporting(E_ALL);
 
 /* START - Required Config */
-$configs['from_name']				= 'AQ8 System® | EMS Training';					// Your name will be used in emails
+$configs['from_name']				= 'EMS Training | Deutschland';					// Your name will be used in emails
 $configs['from_address']			= 'info@aq8system.com';							// Your email address used to send emails
-$configs['to_addresses']			= array('info@aq8system.com');			// The addresse(s) where to send all the requests
+$configs['to_addresses']			= array('info@aq8system.com');					// The addresse(s) where to send all the requests
 /* END - Required Config */
 
 /* START - Optional Configs */
-$configs['lang_path']				= 'lang/en.json';								// Path to the language file
+$configs['lang_path']				= 'lang/de.json';								// Path to the language file
 $configs['recaptcha_enabled']		= true;											// Is Google reCAPTCHA enabled
 $configs['recaptcha_secret']		= '6LcfjFAUAAAAALHvwGQq6Jgo7hFkWESx7Tj2qAT7';	// Google reCAPTCHA secret key
-$configs['success_link']			= 'thankyou.html';							// Link of a custom success page
+$configs['success_link']			= 'gluckwunsch.html';							// Link of a custom success page
 $configs['curl_enabled']			= true;											// Set to false if your server has cURL disabled
 // Generic email configs
 $configs['email_to_owner']			= true;											// Send the email with informations to the site owner?
-$configs['email_confirm']			= false;											// Send a confirm email to user?
+$configs['email_confirm']			= false;										// Send a confirm email to user?
 $configs['email_confirm_html_copy']	= false;										// Add a HTML copy of the data to the confirm email?
 $configs['cc_addresses']			= array();										// The cc addresse(s)
-$configs['bcc_addresses']			= array('backup@aq8system.com');										// The bcc addresse(s)
-$configs['user_ip_address']			= false;										// Do you want to add in the email the IP address of the user who filled in the Form?
+$configs['bcc_addresses']			= array('backup@aq8system.com');				// The bcc addresse(s)
+$configs['user_ip_address']			= true;											// Do you want to add in the email the IP address of the user who filled in the Form?
 // SMTP configs
 $configs['smtp_enabled']			= false;										// Is the SMTP email sending enabled? (if not, PHP mail server will be used)
 $configs['smtp_host']				= '';											// Ip of the SMTP host server
--- contactme/js/contactme-config.js
+++ contactme/js/contactme-config.js
@@ -6,6 +6,6 @@
  */
 
 // Language options
-var cm_lang_path = 'contactme/lang/en.json';			// Choose the language of the error messages
-var cm_datepicker_lang = 'en';							// Choose the language of the date picker plugin
-var cm_dropdown_lang = 'en';							// Choose the language of the dropdown plugin
+var cm_lang_path = 'contactme/lang/de.json';			// Choose the language of the error messages
+var cm_datepicker_lang = 'de';							// Choose the language of the date picker plugin
+var cm_dropdown_lang = 'de';							// Choose the language of the dropdown plugin
